db56221dbd749eb4e996fde5e4496f00067714b8
MarinaSigida <sigida.web@gmail.com>
2024-04-12 10:34:20 +0200

    routing

---
+++ tp-shi-fou-mi/src/pages/Game.jsx
@@ -0,0 +1,14 @@
+const Game = () => {
+  return (
+    <main>
+      <h1>Game</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
+        laboriosam placeat incidunt rem illum animi nemo quibusdam quia
+        voluptatum voluptate.
+      </p>
+    </main>
+  );
+};
+
+export default Game;
---
+++ tp-shi-fou-mi/src/pages/Results.jsx
@@ -0,0 +1,17 @@
+const Results = () => {
+  return (
+    <main>
+      <h1>Results</h1>
+      <br />
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+        <li>Product 4</li>
+        <li>Product 5</li>
+      </ul>
+    </main>
+  );
+};
+
+export default Results;
